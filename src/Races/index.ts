0aabfa2d753a9c4fb5584da517b56809bbf42dd2
caiobacode <caioodsf@gmail.com>
2023-02-24 16:35:47 -0300

    criando subclasses da class "Race"

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,3 +1,9 @@
 import Race from './Race';
+import Halfling from './Halfling';
+import Orc from './Orc';
+import Dwarf from './Dwarf';
+import Elf from './Elf';
 
 export default Race;
+
+export { Dwarf, Elf, Halfling, Orc}
