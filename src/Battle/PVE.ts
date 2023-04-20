7bb71449c0c1be23a1cbd715a7954e231e555e4d
caiobacode <caioodsf@gmail.com>
2023-04-20 16:27:47 -0300

    feat: criando classe PVE - req 11 (while muito apelao)

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,22 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVE extends Battle {
+  constructor(public player: Fighter, public enemies: Fighter[]) {
+    super(player);
+  }
+
+  fight(): number {
+    while (
+      this.player.lifePoints !== -1
+      && this.enemies.every((e) => e.lifePoints !== -1)
+    ) {
+      this.enemies.forEach((e) => {
+        e.attack(this.player);
+        this.player.attack(e);
+      });
+    }
+    const fightResult = super.fight();
+    return fightResult;
+  }
+}
